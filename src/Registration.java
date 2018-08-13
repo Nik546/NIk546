5b6564884560386b4e3baee7527a4e983d25b183
Nik546 <NikhilTalluri@my.unt.edu>
2018-08-13 18:18:01 -0400

    initial commit

---
+++ src/Registration.java
@@ -0,0 +1,45 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Registration extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter printWriter=resp.getWriter();
+        String fname=req.getParameter("firstname");
+        String lname=req.getParameter("lastname");
+        String mailid=req.getParameter("mailid");
+        String gender=req.getParameter("gender");
+        String usname=req.getParameter("uname");
+        String password=req.getParameter("password");
+
+//        System.out.println(fname+lname+mailid+gender+usname+password);
+        if(fname==null || fname=="first name" || lname=="last name" || lname==null || mailid==null || gender==null || usname==null || password==null )
+        {
+//            RequestDispatcher requestDispatcher=req.getRequestDispatcher("/Registrate.html");
+//            requestDispatcher.forward(req,resp);
+
+
+        }
+        else
+        {
+            ServletContext servletContext=getServletContext();
+            servletContext.setAttribute("uname1",usname);
+            servletContext.setAttribute("password1",password);
+
+            RequestDispatcher requestDispatcher=req.getRequestDispatcher("/login.html");
+            requestDispatcher.forward(req,resp);
+        }
+
+
+
+            }
+
+}
---
+++ src/login.java
@@ -0,0 +1,39 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class login extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext=getServletContext();
+        String  s=(String)servletContext.getAttribute("uname1");
+        String k=(String)servletContext.getAttribute("password1");
+
+        if(s.equals(req.getParameter("uname"))&&k.equals(req.getParameter("password")))
+        {
+            PrintWriter printWriter=resp.getWriter();
+            printWriter.print("<html><body>");
+            printWriter.print("<h1>Welcome to main page</h1>"+req.getParameter("uname"));
+            printWriter.print("</body></html>");
+        }
+        else
+        {
+            PrintWriter printWriter=resp.getWriter();
+
+            printWriter.print("<html><body>");
+            resp.getWriter().print("<font color='red'> <b>Incorrect name or password</b></font>");
+
+            printWriter.print("</body></html>");
+            RequestDispatcher requestDispatcher=req.getRequestDispatcher("/login.html");
+            requestDispatcher.include(req,resp);
+
+        }
+    }
+
+
+}
